cc31fdbe3ceb5257e0994e39234cda4c7d9040d2
김영우 <ywkim0114@riskzero.kr>
2024-01-24 17:12:49 +0900

    텝 모듈 생성 적용

--- premiumgolf/js/common.js
+++ premiumgolf/js/common.js
@@ -12,3 +12,45 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+function initTabs(tabSetId) {
+  const tabSet = document.querySelector(`#${tabSetId}`);
+  const tabButtons = tabSet.querySelectorAll(".tab-btn");
+  function changeTab(tabId) {
+    // 모든 콘텐츠 숨기기
+    const tabContents = tabSet.querySelectorAll(".cont > div");
+    tabContents.forEach((content) => {
+      // console.log("content", content);
+      content.classList.remove("active");
+    });
+
+    // 클릭된 탭 버튼을 활성화
+    tabButtons.forEach((button) => {
+      button.classList.remove("active");
+    });
+
+    // 선택한 탭 콘텐츠를 보이게 하고 해당 탭 버튼을 활성화
+    const selectedTabContent = tabSet.querySelector(
+      `.cont div[data-tab="${tabId}"]`
+    );
+    const selectedTabButton = tabSet.querySelector(
+      `.tab-btn[data-tab="${tabId}"]`
+    );
+    console.log("selectedTabContent", selectedTabContent);
+    console.log("selectedTabButton", selectedTabButton);
+    if (selectedTabContent && selectedTabButton) {
+      console.log("selectedTabContent", selectedTabContent);
+      console.log("selectedTabButton", selectedTabButton);
+      selectedTabContent.classList.add("active");
+      selectedTabButton.classList.add("active");
+    }
+  }
+
+  // 각 탭 버튼에 이벤트 리스너 추가
+  tabButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const tabId = this.getAttribute("data-tab");
+      changeTab(tabId);
+    });
+  });
+}
